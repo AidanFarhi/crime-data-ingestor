49bc28da7c92b6c10cea287c86f4025e9906b87d
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-03-24 18:38:43 -0400

    updated code to make calls asyncronously resulting in increased speed and tested locally

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import boto3
-import requests
+import aiohttp
+import asyncio
 import json
 import snowflake.connector as snowflake_connector
 from dotenv import load_dotenv
@@ -15,23 +16,30 @@ def get_list_of_zip_codes(conn):
 	conn.close()
 	return results
 
-def get_results_for_zipcodes(zipcodes, api_key):
-	url = 'https://crime-data-by-zipcode-api.p.rapidapi.com/crime_data'
-	headers = {
-		'X-RapidAPI-Key': api_key,
-		'X-RapidAPI-Host': 'crime-data-by-zipcode-api.p.rapidapi.com'
-	}
-	results = []
-	for zip in zipcodes:
-		response = requests.get(url=url, headers=headers, params={'zip': zip})
-		if response is not None:
-			try:
-				data = response.json()
-				if data['success'] == True:
-					results.append(response.json())
-			except Exception as e:
-				print(e)
-	return results
+async def get_result_for_zipcode(zipcode, session, url, headers):
+    async with session.get(url=url, headers=headers, params={'zip': zipcode}) as response:
+        # TODO: Figure out why some reponses fail to return a valid JSON
+        try:
+            data = await response.json()
+            if data['success'] == True:
+                return data
+        except Exception as e:
+            print(e)
+    return None
+
+async def get_results_for_zipcodes(zipcodes, api_key):
+    url = 'https://crime-data-by-zipcode-api.p.rapidapi.com/crime_data'
+    headers = {
+        'X-RapidAPI-Key': api_key,
+        'X-RapidAPI-Host': 'crime-data-by-zipcode-api.p.rapidapi.com'
+    }
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for zipcode in zipcodes:
+            tasks.append(get_result_for_zipcode(zipcode, session, url, headers))
+        results = await asyncio.gather(*tasks)
+        return [r for r in results if r is not None]
+
 
 def load_results_to_s3(client, results, bucket_name):
 	for i, obj in enumerate(results):
@@ -61,6 +69,8 @@ def main(event, context):
 		schema=os.getenv('SCHEMA')
 	)
 	zipcodes = get_list_of_zip_codes(conn)
-	results = get_results_for_zipcodes(zipcodes, api_key)
+	loop = asyncio.new_event_loop()
+	asyncio.set_event_loop(loop)
+	results = loop.run_until_complete(get_results_for_zipcodes(zipcodes, api_key))
 	load_results_to_s3(client, results, bucket_name)
 	return {'statusCode': 200}
